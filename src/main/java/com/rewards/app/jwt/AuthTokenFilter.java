d13c0d6991e09d889794523ba6ee08e35c3b54a5
sanals <5sanal5z5@gmail.com>
2024-07-18 20:17:34 +0530

    filter added

---
+++ src/main/java/com/rewards/app/jwt/AuthTokenFilter.java
@@ -0,0 +1,50 @@
+package com.rewards.app.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = jwtUtils.getJwtFromHeader(request);
+            if(jwt != null && jwtUtils.validateJwtToken(jwt)){
+                String username = jwtUtils.getUsernameFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                System.out.println("userDetails.getAuthorities(): "+userDetails.getAuthorities());
+
+                // enhancing the authenticationToken with additional details that we are getting from request. Eg:- Session ID
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // Setting the security context. i.e. effectively authenticating the user for duration of the request.
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e){
+            System.out.println("doFilterInternal error"+e);
+        }
+    }
+}
